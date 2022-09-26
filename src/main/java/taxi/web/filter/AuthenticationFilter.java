8f9dffc86b02753129c545c97aacbf217fbf207b
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-09-26 14:45:33 +0300

    added controllers and filter implementation

---
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -0,0 +1,33 @@
+package taxi.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AuthenticationFilter implements Filter {
+    private Set<String> allowedUrls = new HashSet<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        allowedUrls.add("/login");
+        allowedUrls.add("/drivers/add");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpRequest.getSession();
+        Long driverId = (Long) session.getAttribute("driver_id");
+        if (driverId == null && !allowedUrls.contains(httpRequest.getServletPath())) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+            return;
+        }
+        filterChain.doFilter(httpRequest, httpResponse);
+    }
+}
